8f4202f7ff8d5c25abfea5810f35f9bd7df6ee33
Sarulatha28 <sarulathav05@gmail.com>
2025-02-25 15:44:59 +0530

    my portfolio

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import Blog from './blog'
+import './App.css';
+function App() {
+  return (
+    <Blog/> 
+  );
+}
+
+export default App;
---
+++ src/blog.js
@@ -0,0 +1,17 @@
+import './App.css';  
+function Blog  () {
+    return(
+        <div className='header'>
+            <div className='navbar'>
+                <h2>Sarulatha</h2>
+                </div> 
+                <div className='button'>
+                <button>Home</button>
+                <button>contact</button>
+                </div>
+
+
+        </div>
+    )
+}
+export default Blog;
